c57c62fba61ca92067d1123069c6d2c603caf1c5
Anton Mihaylov <antonmmihaylov@gmail.com>
2022-07-23 19:18:28 +0300

    lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,14 @@
 /** @type{import("eslint").ESLint.ConfigData}*/
-module.exports = {
+const config = {
   extends: ['antomic/recommended', 'antomic/react', 'prettier'],
   parserOptions: {
     project: './tsconfig.json',
   },
   settings: { 'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] } } },
+  rules: {
+    'unicorn/filename-case': 'off',
+  },
 }
+
+// eslint-disable-next-line no-undef
+module.exports = config
